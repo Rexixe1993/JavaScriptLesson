0e79af871a73c5060e3f575bb1a32b5954efa00a
Rexixe1993 <417378369@qq.com>
2020-08-06 00:42:48 +0800

    8.5日比较字符串和0~9数字的作业

---
+++ 学习小组/JS/8.5 作业.js
@@ -0,0 +1,21 @@
+function words(a) {
+    for (let b = 0; b <= 9; b++) {
+        if (a == b) {
+            console.log("true");
+            return
+        }
+    }
+    console.log("false");
+    return;
+}
+
+words(0);
+words(9);
+words("0");
+words("9");
+words("啊");
+words("A");
+words("ab");
+words(12);
+words("是吧");
+
