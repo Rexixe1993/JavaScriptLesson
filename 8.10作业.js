50b0a8a94fb3c08b5c93a7307ff2c11eb5a99430
Rexixe1993 <417378369@qq.com>
2020-08-11 02:49:56 +0800

    8.10作业

--- 8.10作业.js
+++ 8.10作业.js
@@ -1,16 +1,29 @@
-function words(a) {
+// 输入任意年份，判断是不是闰年（8.10日作业）
+// 输入任意字符，判断是否为0~9中任意整数。
+
+
+function year(y) {
+
+    if (typeof (y) == "number") {
+
+        if (y % 4 == 0 && y % 100 !== 0 || y % 400 == 0) {
+            console.log(y + "年是闰年。")
+            return;
+        } else {
+            console.log(y + "年是平年。");
+        }
+    } else {
+        console.log("输入有误！");
+    }
+}           // 判断输入的年份是不是闰年
+
+function num(a) {
+
     for (let b = 0; b <= 9; b++) {
-        if (a == toString(b)) {
-            console.log("true");
-            return
+        if (a == String(b)) {
+            console.log("yes");
+            return;
         }
     }
-    console.log("false");
-}
-
-words("是");
-
-
-输入任意年份，判断是不是闰年（8.10日作业）
-
-
+    console.log("no");
+}            // 判断输入内容是否属于0~9任意整数。
