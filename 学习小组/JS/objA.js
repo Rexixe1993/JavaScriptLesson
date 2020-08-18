abb7098a353882b0d2d7825b7cd0cc3d045b4fb2
Rexixe1993 <417378369@qq.com>
2020-08-19 02:04:40 +0800

    1

--- 学习小组/JS/objA.js
+++ 学习小组/JS/objA.js
@@ -3,18 +3,14 @@ var abc = {
     b: 1,
     c: function () {
         for (let d = 0; d <= 9; d++) {
-            if (d === 5) {
+            if (d % 2 !== 0) {
                 console.log("hahahahaha");
-                return;
+                continue;
             }
             console.log(d + "riririiririr");
         }
     }
-};
-
-function e() {
-    abc.c();
-}
+};   //介是一个对象，用来实现一些奇怪的功能，可以直接引用它。
 
 
-e();
+abc.c();
