081dab87e69a2715422a3b27d1bb9f833437a87e
Rexixe1993 <417378369@qq.com>
2020-08-19 00:57:52 +0800

    对象练习

--- 学习小组/JS/objA.js
+++ 学习小组/JS/objA.js
@@ -3,7 +3,7 @@ var abc = {
     b: 1,
     c: function () {
         for (let d = 0; d <= 9; d++) {
-            if (d = 5) {
+            if (d === 5) {
                 console.log("hahahahaha");
                 return;
             }
