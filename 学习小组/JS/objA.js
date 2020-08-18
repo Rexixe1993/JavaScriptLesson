b34c6c225ddcd6367eeff80295a16fbc368d8c36
Rexixe1993 <417378369@qq.com>
2020-08-19 00:57:20 +0800

    对象

---
+++ 学习小组/JS/objA.js
@@ -0,0 +1,20 @@
+var abc = {
+    a: "",
+    b: 1,
+    c: function () {
+        for (let d = 0; d <= 9; d++) {
+            if (d = 5) {
+                console.log("hahahahaha");
+                return;
+            }
+            console.log(d + "riririiririr");
+        }
+    }
+};
+
+function e() {
+    abc.c();
+}
+
+
+e();
